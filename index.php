827a136373b6d54efbec8106d6a02c0a7a6397f9
Satyam-2001 <lohiyasatyam@gmail.com>
2021-11-26 16:09:37 +0530

    Add action

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
             </form>
         </div>
         <div class="form-container sign-in-container">
-            <form action="validation.php" method="post">
+            <form action="https://loginsysteminphp.herokuapp.com/validation.php" method="post">
                 <h1 style="padding-bottom:40px;">Sign in</h1>
                 <input type="text" name="username" placeholder="Username" required/>
                 <input type="password" name="password" placeholder="Password" required/>
